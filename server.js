049b6677201b3296577377cd9c6176c5986949fc
Noel Cruz <noel.j.cruz@outlook.com>
2019-09-09 21:40:48 -0700

    Added to the server.js file and created friendsArray with initial object in the friends.js file.

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var bodyParser = require('body-parser');
 var express = require('express');
+
 var app = express();
+var PORT = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
-
-app.listen(3000);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -16,4 +16,8 @@ app.use(function (req, res) {
     res.setHeader('Content-Type', 'text/plain');
     res.write('you posted:\n');
     res.end(JSON.stringify(res.body, null, 2));
+});
+
+app.listen(PORT, function () {
+    console.log('App listening on PORT: ' + PORT);
 });
