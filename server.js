aec6458ad4875ef8d4595f7c3acc2600b86eb06b
Noel Cruz <noel.j.cruz@outlook.com>
2019-09-09 22:12:42 -0700

    Added HTML routes.

--- server.js
+++ server.js
@@ -5,19 +5,10 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(bodyParser.json());
 
-app.use(function (req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('you posted:\n');
-    res.end(JSON.stringify(res.body, null, 2));
-});
+require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, function () {
     console.log('App listening on PORT: ' + PORT);
